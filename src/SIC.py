f72ed7cc960aef3b1634a99014fd2766d5390217
ariyn <himnowxz@gmail.com>
2016-11-12 13:24:23 +0900

    every function will be stored in infos.py

--- src/SIC.py
+++ src/SIC.py
@@ -1,5 +1,7 @@
 #!python3
 from infos import *
+from Device import Printer
+
 RegisterSize = 24 #bits
 IntegerSize = 24 #Bits
 CharacterSize = 24 #Bits
@@ -43,45 +45,6 @@ instructions = {
 	"DUMP":0xF1,
 	"LOAD":0xF2
 }
-
-def decodeBits(bits, unsigned=False, zf=False, length=24):
-	if zf:
-		bits = zeroFill(bits, length)
-
-	if len(bits) < length:
-		raise WrongBitSize
-
-	value = int(bits, 2)
-
-	if not unsigned and bits[0] == "1":
-		newBits = "".join(["0" if i == "1" else "1" for i in bits])
-
-		value = -(int(newBits, 2)+1)
-		
-	if length < len(bits):
-		raise OverflowError("TOO LONG INT")
-
-	return value
-
-def encodeBits(value, unsigned=False, length=24):
-	if value < 0 and not unsigned:
-		bits = ("{0:0%db}"%length).format(value & (2**length))
-	else:
-		bits = ("{0:0%db}"%length).format(value)
-
-
-	if length < len(bits):
-		raise OverflowError("TOO LONG INT")
-
-	return bits
-
-def signExtend(bits, length=24):
-	val = bits[0] * (length - len(bits))
-	bits = val + bits
-	return bits
-
-def zeroFill(bits, length=24):
-	return "0" * (length - len(bits)) + bits
 
 class SIC:
 	# Interger 24Bits
--- src/infos.py
+++ src/infos.py
@@ -47,3 +47,60 @@ class Register:
 			return True
 		else:
 			return False
+def loadBinFile(file):
+	retVal = []
+	binArr = True
+	while True:
+		binArr = bytearray(file.read(3))
+		number = 0
+		for i in range(0, len(binArr)):
+			number = number << 8
+			number += binArr[i]
+			# print(binArr[i], number)
+		
+		if not binArr:
+			break
+		# print(binArr, number)
+		# print(encodeBits(number))
+		retVal.append(encodeBits(number))
+	
+	return retVal
+	
+def decodeBits(bits, unsigned=False, zf=False, length=24):
+	if zf:
+		bits = zeroFill(bits, length)
+
+	if len(bits) < length:
+		raise WrongBitSize
+
+	value = int(bits, 2)
+
+	if not unsigned and bits[0] == "1":
+		newBits = "".join(["0" if i == "1" else "1" for i in bits])
+
+		value = -(int(newBits, 2)+1)
+		
+	if length < len(bits):
+		raise OverflowError("TOO LONG INT")
+
+	return value
+
+def encodeBits(value, unsigned=False, length=24):
+	if value < 0 and not unsigned:
+		bits = ("{0:0%db}"%length).format(value & (2**length))
+	else:
+		bits = ("{0:0%db}"%length).format(value)
+
+
+	if length < len(bits):
+		raise OverflowError("TOO LONG INT")
+
+	return bits
+
+def signExtend(bits, length=24):
+	val = bits[0] * (length - len(bits))
+	bits = val + bits
+	return bits
+
+def zeroFill(bits, length=24):
+	return "0" * (length - len(bits)) + bits
